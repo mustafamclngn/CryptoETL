cc681cf43241748f8924cdb3bc96f37a31c0271f
Mustafa <mmmmacalangan@gmail.com>
2026-04-21 19:38:34 +0800

    added image_url to columns

--- etl/transform.py
+++ etl/transform.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime, timezone
 
 COLUMNS = [
-    "id", "symbol", "name",
+    "id", "symbol", "name", "image",
     "current_price", "market_cap",
     "total_volume", "price_change_percentage_24h"
 ]
@@ -12,16 +12,15 @@ def transform(raw: list[dict]) -> pd.DataFrame:
 
     df = df.rename(columns={
         "id": "coin_id",
+        "image": "image_url",
         "price_change_percentage_24h": "price_change_24h"
     })
 
-    # convert to numeric, error handling
     df["current_price"]    = pd.to_numeric(df["current_price"],    errors="coerce")
     df["market_cap"]       = pd.to_numeric(df["market_cap"],       errors="coerce")
     df["total_volume"]     = pd.to_numeric(df["total_volume"],     errors="coerce")
     df["price_change_24h"] = pd.to_numeric(df["price_change_24h"], errors="coerce")
 
-    # timestamp
     df["fetched_at"] = datetime.now(timezone.utc)
 
     print(f"[TRANSFORM] Cleaned {len(df)} rows.")
